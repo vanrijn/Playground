a822c784816027d5e14bdc980a31d5907b611d21
Jason 'vanRijn' Kasper <vR@movingparts.net>
2016-02-11 22:36:26 -0500

    Fixed class names, fixed remove method

--- linkedlist.py
+++ linkedlist.py
@@ -1,81 +1,92 @@
 class LinkedListNode:
 	def __init__(self, value):
-		self.value = value
-		self.childNode = None
+		self._value = value
+		self._childNode = None
 		print "Hi there! I'm a LinkedListNode and my value is: %s" % value
 
 	def childNode(self):
-		return self.childNode
+		return self._childNode
 
 	def setChildNode(self, node):
-		self.childNode = node
+		self._childNode = node
 
 	def value(self):
-		return self.value
+		return self._value
 
 	def setValue(self, value):
-		self.value = value
+		self._value = value
 
 class LinkedList:
 	def __init__(self):
-		self.rootNode = None
-		self.lastNode = self.rootNode
-		self.size = 0
+		self._rootNode = None
+		self._lastNode = None
+		self._size = 0
 
 	def push_back(self, value):
 		node = LinkedListNode(value)
 
 		# If this is the first time adding a node, this will be our rootNode.
 		# Otherwise, set rootNode to be the parentNode
-		if self.rootNode is None:
-			self.rootNode = node
+		if self._rootNode is None:
+			self._rootNode = node
 		else:
-			self.lastNode.setChildNode(node)
+			self._lastNode.setChildNode(node)
 
-		self.lastNode = node
-		self.size += 1
+		self._lastNode = node
+		self._size += 1
 
 	def print_all(self):
-		node = self.rootNode
+		print "My size is: %d" % self._size
+		node = self._rootNode
 		while node is not None:
 			print "Node value: %s" % str(node.value())
 			node = node.childNode()
 
 	def remove(self, value):
-		if self.size == 0:
+		if self._size == 0:
 			print "Can't remove value %s. I'm empty" % str(value)
 			return
 
-		if self.size == 1:
-			self.rootNode.setValue(None)
+		if self._size == 1:
+			self._rootNode.setValue(None)
 			return
 
-		node = self.rootNode
+		node = self._rootNode
 		parentNode = None
 
 		while node is not None:
 			if node.value() == value:
+				print "Found node."
 				break
 
 			parentNode = node
 			node = node.childNode()
 
 		# We can only remove a node if we found one
-		if node.value() != value:
+		if node is None or node.value() != value:
 			print "Sorry, couldn't remove value: %s (not found)" % str(value)
 			return
 
-		parentNode.setChildNode(node.childNode())
+		nextNode = node.childNode()
 
-		self.size -= 1
+		# If we're removing our root node, update it to be the next node
+		if parentNode is None:
+			print ("Removing root node %s and updating to new node: %s" %
+			       (str(self._rootNode.value()), str(nextNode.value())))
+			self._rootNode = nextNode
+		else:
+			print ("Parent node: %s, now joining to new child node: %s" %
+			       (str(parentNode.value()), str(nextNode.value())))
+			parentNode.setChildNode(nextNode)
+
+		self._size -= 1
 
 
 list = LinkedList()
 
-# this will fail
 print "Trying to remove something from an empty list"
 list.remove("something from an empty list")
-list.print_all()
+print
 
 print "Populating list"
 list.push_back(1)
@@ -85,19 +96,18 @@ list.push_back(4)
 list.push_back(5)
 list.push_back("egg")
 list.print_all()
+print
 
-# this will fail because it's not there
 print "Trying to remove something not in the list..."
 list.remove("something not in the list")
 list.print_all()
+print
 
-
-# this should succeed
 print "Removing first node (1)"
 list.remove(1)
 list.print_all()
+print
 
-# this should succeed
 print "Removing middle node (4)"
 list.remove(4)
 list.print_all()
